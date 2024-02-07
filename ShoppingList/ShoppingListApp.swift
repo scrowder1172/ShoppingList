af167bcecfc66bca35aa5abf18cd69480073d9c0
Scott Crowder <scrowder@gmail.com>
2024-02-07 13:14:53 -0600

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by SCOTT CROWDER on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
